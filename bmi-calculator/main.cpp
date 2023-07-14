54c9920424a75c15173fce5d881fd52b77b33ebf
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-13 21:36:16 -0500

    First Attempt

--- bmi-calculator/main.cpp
+++ bmi-calculator/main.cpp
@@ -2,13 +2,29 @@
 //  main.cpp
 //  bmi-calculator
 //
-//  Created by Hope Alvarez on 12/07/23.
+//  Created by Alvaro Alvarez on 12/07/23.
 //
 
 #include <iostream>
+#include <math.h>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+//    variables
+    float weight = 0, height = 0, result = 0;
+    
+//    data request
+    cout << "Body Mass Index Calculator (BMI)\n";
+    cout << "Enter your weight(kg): ";
+    cin >> weight;
+    cout << "Now, enter your height(m): ";
+    cin >> height;
+    
+//    Calculating BMI
+    result = weight / pow(height, 2);
+    cout << "Your BMI is: " << setprecision(4) << result << " kg/m^2\n";
+
     return 0;
 }
